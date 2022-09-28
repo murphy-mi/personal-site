24701ae9703c1c0e523c4796e67d48ac25ef4455
murphy-mi <murphymax1@gmail.com>
2022-09-27 20:47:12 -0400

    header sizing & resume update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,28 @@
-import './header.css'
-import HeaderSocials from './HeaderSocials'
-import Resume from './Resume'
-import MEBW from '../../assets/me-transparent-bw.png'
+import "./header.css";
+import HeaderSocials from "./HeaderSocials";
+import Resume from "./Resume";
+import MEBW from "../../assets/me-transparent-bw.png";
 // import ME from '../../assets/me-transparent.png'
 
 function Header() {
-    return (
-        <header>
-            <div className="container header__container">
-                <h5>
-                    Hello I'm
-                </h5>
-                <h1>Max Murphy</h1>
-                <h5 className="text-light">Fullstack Developer</h5>
-                <Resume />
-                <HeaderSocials />
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Max Murphy</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <Resume />
+        <HeaderSocials />
 
-                <div className="me">
-                    <img src={MEBW} alt="Max Murphy headshot" />
-                </div>
-                <a href="#contact" className='scroll__down'>Scroll Down</a>
-            </div>
-        </header>
-    );
+        <div className="me">
+          <img src={MEBW} alt="Max Murphy headshot" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
